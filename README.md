79de4e0cde0fe038eb4b5f1090aa931071cd25da
Christian Fobel <christian@fobel.net>
2014-03-19 09:16:42 -0400

    Add auto git-describe version for Python packag...
    Add automatic Python package versioning using `git-describe` through the
    `version.py` module from [here][1].
    
    A Python package source distribution can be built using the following
    command:
    
        python setup.py sdist
    
    __NB__ The version strings generated by `version.getVersion()` conform
    to [PEP386][2].
    
    [1]: https://gist.github.com/mina86/8782771
    [2]: http://www.python.org/dev/peps/pep-0386

---
+++ README.md
@@ -0,0 +1,6 @@
+# `application-repository.jsonrpc` #
+
+This project is a fork of the `python-json-rpc` package hosted [here][1],
+written by Jan-Klaas Kollhof.
+
+[1]: http://json-rpc.org/wiki/python-json-rpc
--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 
 from distutils.core import setup
 
-setup(name = "jsonrpc",
-    version = "0.01",
-    description = "A json-rpc package which implements JSON-RPC over HTTP.",
-    keywords = "JSON RPC",
-    author = "Jan-Klaas Kollhof",
-    url = "http://json-rpc.org/wiki/python-json-rpc",
-    license = "LGPL",
-    long_description = """""",
-    packages = ['jsonrpc']
-)
+import version
+
+
+setup(name='application_repository.jsonrpc',
+      version=version.getVersion(),
+      description='A json-rpc package which implements JSON-RPC over HTTP.',
+      keywords='JSON RPC',
+      author='Christian Fobel',
+      author_email='christian@fobel.net',
+      url='https://github.com/wheeler-microfluidics/application_repository.jsonrpc',
+      license='LGPL',
+      packages=['jsonrpc'])
